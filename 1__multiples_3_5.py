3ff9086d66a036e8208cfff9790dcb8245929ae0
Yameen Rasheed <yameenr@gmail.com>
2016-01-12 01:32:38 +0500

    Add problems 1 and 2

---
+++ 1__multiples_3_5.py
@@ -0,0 +1,19 @@
+'''
+ If we list all the numbers below 10 that are multiples of 
+ 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ Find the sum of all the multiples of 3 or 5 below 1000.
+'''
+
+def main():
+    a = []
+    sum = 0
+    for x in range(1,1000):
+        if(x % 3 == 0 or x %5 == 0):
+          a.append(x)
+    for item in a:
+        sum += item
+    print ("sum of all multiples of 3 and 5 is {:d}".format(sum))   
+ 
+if __name__ == '__main__':
+    main()
+
